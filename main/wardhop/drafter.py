9226a555540c76bf96635bddfd33dfa565daf229
Derek Dang <dqdang17@gmail.com>
2020-11-13 01:49:38 -0800

    Fix monkey issues

--- main/wardhop/drafter.py
+++ main/wardhop/drafter.py
@@ -35,12 +35,17 @@ class ChampionDatabase():
     def get_all_champ_images(self):
         images = {}
         for champ in self.static_champ_list['data']:
-            images[champ] = self.static_champ_list['data'][champ]['image']
+            image = self.static_champ_list['data'][champ]['image']
+            if champ == "MonkeyKing":
+                image['full'] = "Wukong." + image['full'].split(".")[1]
+            images[champ] = image
         return images
 
     def get_all_champ_names(self):
         names = []
         for champ in self.static_champ_list['data']:
+            if champ == "MonkeyKing":
+                champ = "Wukong"
             names.append(champ)
         return names
 
@@ -48,8 +53,13 @@ class ChampionDatabase():
         key = ''.join(e for e in key if e.isalpha())
         for champ in self.static_champ_list['data']:
             champ_lower_case = champ.lower()
+            if champ_lower_case == "monkeyking":
+                champ_lower_case = "wukong"
             if champ_lower_case.startswith(key):
-                return self.static_champ_list['data'][champ]['image']
+                image = self.static_champ_list['data'][champ]['image']
+                if champ_lower_case == "wukong":
+                    image['full'] = "Wukong." + image['full'].split(".")[1]
+                return image
         return None
 
     def search(self, key):
